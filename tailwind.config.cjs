03821934fa718b1d22276452051adaaf7d92768b
jimbo23 <kiefersoon@gmail.com>
2022-11-28 20:29:21 +0800

    Setup custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+
+    fontFamily: {
+      sans: ["Noto Sans", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      colors: {
+        blue: "#2F80ED",
+        pink: "#ff49db",
+        orange: "#ff7849",
+        "gray-dark": "#273444",
+        gray: "#8492a6",
+        "gray-light": "#d3dce6",
+      },
+    },
   },
   plugins: [],
 };
