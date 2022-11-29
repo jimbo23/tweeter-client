3460a174d222cf41797017662789a215d1504d06
jimbo23 <kiefersoon@gmail.com>
2022-11-29 20:53:12 +0800

    configure for absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// @ts-ignore
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: [
+      // @ts-ignore
+      { find: "@kiefer-tweeter", replacement: path.resolve(__dirname, "/src") },
+    ],
+  },
+  plugins: [react()],
+});
