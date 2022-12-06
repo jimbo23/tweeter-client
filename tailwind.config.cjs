39b62a7295106e3fc359fa90d971aa0b653aa35c
jimbo23 <kiefersoon@gmail.com>
2022-12-06 22:15:32 +0800

    Update tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       sm: "480px",
       md: "768px",
       lg: "976px",
-      xl: "1440px",
+      xl: "1076px",
     },
     fontFamily: {
       sans: ["Noto Sans", "sans-serif"],
@@ -14,20 +14,20 @@ module.exports = {
     },
     extend: {
       fontSize: {
-        '2xs': "0.75rem", // 12px,
+        "2xs": "0.75rem", // 12px,
         xs: "0.875rem", // 14px,
         sm: "1rem", // 16px
         md: "1.25rem", // 20px
         lg: "1.5rem", // 24px
         xl: "1.75rem", // 28px
-        '2xl': "2rem", // 32px
+        "2xl": "2rem", // 32px
       },
-      spacing: {
-        128: "32rem",
-        144: "36rem",
+      flexBasis: {
+        "4/10": "40%",
+        "6/10": "60%",
       },
       borderRadius: {
-        sm: '0.375rem',
+        sm: "0.375rem",
         "4xl": "2rem",
       },
       backgroundColor: {
@@ -38,13 +38,12 @@ module.exports = {
       colors: {
         pink: "#ff49db",
         orange: "#ff7849",
-        "gray-dark": "#273444",
         gray: "#8492a6",
-        "gray-light": "#d3dce6",
+        'bg-gray': '#F2F2F2'
       },
       padding: {
-        '18': '4.5rem',
-      }
+        18: "4.5rem",
+      },
     },
   },
   plugins: [],
